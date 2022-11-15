c6a284e8755b03e51635b95b7bd815ff0b69a75c
Samuele Chiocca <samuele@sighup.io>
2022-11-15 10:35:21 +0100

    feat: update deprecated way to set provider versions

--- modules/aws-k8s-conformance/main.tf
+++ modules/aws-k8s-conformance/main.tf
@@ -1,23 +1,28 @@
-provider "aws" {
-  version = "~> 2.48"
-}
-
-provider "tls" {
-  version = "~> 2.1"
-}
-
-provider "template" {
-  version = "~> 2.1"
-}
-
-provider "random" {
-  version = "~> 2.2"
-}
-
-provider "null" {
-  version = "~> 3.0"
-}
-
-provider "local" {
-  version = "~> 2.0"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 2.48"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 2.1"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "~> 2.1"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 2.2"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
 }
